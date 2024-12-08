316d17002227e7c574e97c5dbe0cda144beac471
eduaugust <eduardo.evangelista2002@gmail.com>
2024-12-08 16:44:19 -0300

    :bug: fix: fix router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
-import CryptoTracker from './components/CryptoTracker';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import News from './components/News';
+import CryptoSparkline from "./components/CryptoSparkline";
+import InfosPage from "./pages/InfosPage";
 
 function App() {
-  const [view, setView] = useState('crypto');
-
   return (
-    <div className="container my-4">
-      <h1 className="text-center text-primary mb-4">Real-Time Tracker</h1>
-      <div className="text-center mb-4">
-        <button className="btn btn-primary mx-2" onClick={() => setView('crypto')}>Crypto Information</button>
-        <button className="btn btn-secondary mx-2" onClick={() => setView('news')}>News Information</button>
-      </div>
-      {view === 'crypto' ? <CryptoTracker /> : <News />}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<InfosPage />} />
+        <Route path="/CryptoSparkline" element={<CryptoSparkline />} />
+      </Routes>
+    </Router>
   );
 }
 
